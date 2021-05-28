9e994ef30d5dba6a12631f6f315baded034f178c
Catherine Tate <60581359+Catherine-Tate@users.noreply.github.com>
2021-05-28 11:06:08 -0400

    started adding some functionality

--- sudoku.py
+++ sudoku.py
@@ -1,2 +1,25 @@
 #sudoku game
+import numpy as np
+
 #written by Catherine Tate
+
+
+"""
+process for creating the board:
+1. generate filled grid
+2. remove some #s (more/less for different difficulties?)
+3. check if grid w/ numbers taken out has only 1 solution
+"""
+
+def makeBoard():
+	board = np.zeros((9,9))
+
+
+
+def printMenu():
+	print("="*16)
+	print("Welcome to SUDOKU")
+	print("="*16)
+	board = makeBoard()
+
+printMenu()
