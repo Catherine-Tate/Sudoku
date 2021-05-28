0bf3988d9231bdb067821ffd81bc10f6821b1116
Catherine-Tate <catherine21tate@gmail.com>
2021-05-28 11:39:02 -0400

    began board generation, printing

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,8 @@
 #sudoku game
 import numpy as np
+import random
 
-#written by Catherine Tate
+#written by Catherine Tate in python 3
 
 
 """
@@ -11,10 +12,22 @@ process for creating the board:
 3. check if grid w/ numbers taken out has only 1 solution
 """
 
+def printBoard(board):
+    print("\t | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |")
+    print("\t " + "-"*37)
+    for i in range(0, 9):
+        print("\t" + str(i) + "|", end = "")
+        for j in range(0, 9):
+            print(" " + str(board[i][j]) + " |", end='')
+        print("")
 def makeBoard():
-	board = np.zeros((9,9))
-
-
+    board = np.zeros((9,9), dtype=int)
+    #make a random set of seed values to start the board off
+    for i in range(1, 10):
+        x = random.randint(0, 8)
+        y = random.randint(0, 8)
+        board[x][y] = i
+    printBoard(board)
 
 def printMenu():
 	print("="*16)
