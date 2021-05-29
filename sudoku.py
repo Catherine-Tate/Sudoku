14782c9dd6a9e1b51db6c0cf6c03a5b4b9e74cf1
Catherine-Tate <catherine21tate@gmail.com>
2021-05-29 12:13:45 -0400

    More work on grid generation. 2nd Square now properly being generated

--- sudoku.py
+++ sudoku.py
@@ -15,7 +15,39 @@ process for creating the board:
 """
 generate the first 3x3 square of the puzzle
 """
-def iPerm(seed):
+
+def boxTwo(board, seed):
+    #fill in the first column of the second box
+    random.shuffle(seed)
+    for i in range(0, 3):
+        board[0][3+i] = seed[i]
+
+    #possible values fo rows 2 and 3 in box 2
+    R2I = list((set(board[0][:3]).union(set(board[2][:3]))) - set(board[0][3:6]))
+    R3I = (set(board[0][:3]).union(set(board[1][:3]))) - set(board[0][3:6])
+    print(R2I, R3I)
+
+    #choose a set of values for row 2 that leaves enough values for row 3
+    while(1):
+        random.shuffle(R2I)
+        R2N = R2I[:3]
+        R3N = R3I - set(R2N)
+        if(len(R3N) == 3):
+            R3N = list(R3N)
+            break
+
+    #print(R2N, R3N)
+    for i in range(0,3):
+        board[1][i+3] = R2N[i]
+
+    #numbers that can be in the 3rd row of the 2nd square
+
+    for i in range(0,3):
+        board[2][i+3] = R3N[i]
+
+    return(board)
+
+def boxOne(seed):
     board = np.zeros((9,9), dtype="int")
     #fill in one 3x3 grid first
     x = 0
@@ -44,12 +76,19 @@ def printBoard(board):
             else:
                 print(" |", end = '')
         print("")
+    print("\t " + "-"*39)
+
 
 def makeBoard():
     #seed value
     seed = random.sample(range(1, 10), 9)
-    board = iPerm(seed)
+    board = boxOne(seed)
     #printBoard(board)
+
+    #potential numbers for box 2 row 1
+    seed2 = seed[3:]
+    board = boxTwo(board, seed2)
+    printBoard(board)
 
     #fill in the next two 3x3 squares
 
